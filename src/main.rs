f5b98dfadea28238c6ef4ae5f7f401b47e465caf
Cloud User <centos@ip-172-31-30-197.eu-west-2.compute.internal>
2022-12-13 10:23:08 +0000

    update

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,87 @@
-// Vaulty - Encrypt/Decrypt with ChaCha20-Poly1305
-// Copyright (c) 2021-2023 Chris Mason <chris@netnix.org>
-//
-// Permission to use, copy, modify, and distribute this software for any
-// purpose with or without fee is hereby granted, provided that the above
-// copyright notice and this permission notice appear in all copies.
-//
-// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+/*
+ * Vaulty - Encrypt/Decrypt with ChaCha20-Poly1305
+ * Copyright (c) 2021-2023 Chris Mason <chris@netnix.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
 
+use std::str;
 use rand_core::{RngCore, OsRng};
 use chacha20poly1305::{aead::{Aead,KeyInit}, ChaCha20Poly1305};
 use zeroize::Zeroize;
 
 fn main() {
-  let mut salt = [0u8; 16];
-  OsRng.fill_bytes(&mut salt);
+  const VAULTY_VERSION: u8 = 0x01;
+  const VAULTY_PREFIX: &str = "$VAULTY;";
 
-  println!("Random Salt is {:x?}", salt);
+  let plaintext = b"Hello World";
+
+  // - ENCRYPT
 
   let mut password = rpassword::prompt_password("Vaulty Password: ").unwrap();
 
-  let mut key = [0u8; 32];
-  let params = scrypt::Params::new(16, 8, 1).unwrap();
+  let mut salt = [0_u8; 16];
+  OsRng.fill_bytes(&mut salt);
+
+  let mut key = [0_u8; 32];
+  let params = scrypt::Params::new(4, 8, 1).unwrap();
   scrypt::scrypt(&password.as_bytes(), &salt, &params, &mut key).unwrap();
 
-  println!("Key is {:x?}", key);
-
-  let mut nonce = [0u8; 12];
+  let mut nonce = [0_u8; 12];
   OsRng.fill_bytes(&mut nonce);
 
-  println!("Nonce is {:x?}", nonce);
-
   let cipher = ChaCha20Poly1305::new(key[..].as_ref().into());
-  let ciphertext = cipher.encrypt(nonce[..].as_ref().into(), b"Hello World".as_ref()).unwrap();
+  let ciphertext = cipher.encrypt(nonce[..].as_ref().into(), &plaintext[..]).unwrap();
 
-  println!("Ciphertext is {:x?}", ciphertext);
+  let x = [&VAULTY_VERSION.to_be_bytes(), &salt[..], &nonce[..], &ciphertext[..]].concat();
 
-  let version = "\u{1}".as_bytes();
+  let mut s = VAULTY_PREFIX.to_owned();
+  s.push_str(&base64::encode(x));
 
-  let x = [&version[..], &salt[..], &nonce[..], &ciphertext[..]].concat();
-
-  println!("All is {:x?}", x);
-
-  let s = base64::encode(x);
-
-  println!("$VAULTY;{}", s);
+  println!("{}", s);
 
   password.zeroize();
   key.zeroize();
+
+  // - DECRYPT
+
+  let mut password = rpassword::prompt_password("Vaulty Password: ").unwrap();
+ 
+  if s.starts_with(VAULTY_PREFIX) {
+    let x = base64::decode(&s[VAULTY_PREFIX.len()..]).unwrap();
+
+    if x[0] == VAULTY_VERSION && x.len() > 29 {
+      let mut salt = [0_u8; 16];
+      salt.copy_from_slice(&x[1..17]);
+
+      let mut key = [0_u8; 32];
+      let params = scrypt::Params::new(4, 8, 1).unwrap();
+      scrypt::scrypt(&password.as_bytes(), &salt, &params, &mut key).unwrap();
+
+      let mut nonce = [0_u8; 12];
+      nonce.copy_from_slice(&x[17..29]);
+
+      let cipher = ChaCha20Poly1305::new(key[..].as_ref().into());
+      let plaintext = match cipher.decrypt(nonce[..].as_ref().into(), &x[29..]) {
+        Ok(v) => v,
+        Err(e) => eprintln!("{}", e)
+      };
+
+      println!("{}", str::from_utf8(&plaintext).unwrap());
+
+      key.zeroize();
+    }
+    password.zeroize();
+  }
 }
 
