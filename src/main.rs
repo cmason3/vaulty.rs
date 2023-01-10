84f7b7a0229c6e8fc33c71a96e067e6edab6270b
Chris Mason <chris.mason@virginmediao2.co.uk>
2023-01-10 01:49:15 +0000

    update vaulty with arguments

--- src/main.rs
+++ src/main.rs
@@ -15,79 +15,107 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-use std::str;
-use std::io::{Read, Write};
-use rand_core::{RngCore, OsRng};
+use std::{io::{Read, Write}, str, cmp, env};
 use chacha20poly1305::{aead::{Aead, KeyInit}, ChaCha20Poly1305};
+use rand_core::{RngCore, OsRng};
 
 fn main() {
   const VAULTY_VERSION: u8 = 0x01;
   const VAULTY_PREFIX: &str = "$VAULTY;";
 
-  let mut input = Vec::new();
-  let mut h = std::io::stdin().lock();
-  h.read_to_end(&mut input).unwrap();
+  let mut mo = 0;
+  let args: Vec<String> = env::args().collect();
 
-  if String::from_utf8_lossy(&input).trim().starts_with(VAULTY_PREFIX) {
-    let s: &String = &String::from_utf8(input).unwrap().split_whitespace().collect();
-    let x = base64::decode(&s[VAULTY_PREFIX.len()..]).unwrap();
-
-    if x[0] == VAULTY_VERSION && x.len() > 29 {
-      let password = rpassword::prompt_password("Vaulty Password: ").unwrap();
-
-      let mut key = [0_u8; 32];
-      let mut salt = [0_u8; 16];
-      salt.copy_from_slice(&x[1..17]);
-
-      derive_key(&password, &mut salt, &mut key, false).unwrap();
-
-      let mut nonce = [0_u8; 12];
-      nonce.copy_from_slice(&x[17..29]);
-
-      let cipher = ChaCha20Poly1305::new(key[..].as_ref().into());
-      match cipher.decrypt(nonce[..].as_ref().into(), &x[29..]) {
-        Ok(v) => {
-          std::io::stdout().write(&v).unwrap();
-          std::io::stdout().flush().unwrap();
-        },
-        Err(_e) => {
-          eprintln!("Error: Unable to Decrypt Ciphertext");
-        }
-      };
+  if args.len() == 2 {
+    if args[1] == "encrypt"[..cmp::min(args[1].len(), 7)] {
+      mo = 1;
     }
-    else {
-      eprintln!("Error: Invalid Vaulty Ciphertext");
+    else if args[1] == "decrypt"[..cmp::min(args[1].len(), 7)] {
+      mo = 2;
+    }
+  }
+
+  if mo > 0 {
+    let mut input = Vec::new();
+    let mut h = std::io::stdin().lock();
+    h.read_to_end(&mut input).unwrap();
+
+    if mo == 1 {
+      let password = rpassword::prompt_password("Vaulty Password: ").unwrap();
+      if password == rpassword::prompt_password("Password Verification: ").unwrap() {
+        let mut key = [0_u8; 32];
+        let mut salt = [0_u8; 16];
+  
+        derive_key(&password, &mut salt, &mut key, true).unwrap();
+  
+        let mut nonce = [0_u8; 12];
+        OsRng.fill_bytes(&mut nonce);
+    
+        let cipher = ChaCha20Poly1305::new(key[..].as_ref().into());
+        let ciphertext = cipher.encrypt(nonce[..].as_ref().into(), &input[..]).unwrap();
+    
+        let x = [&VAULTY_VERSION.to_be_bytes(), &salt[..], &nonce[..], &ciphertext[..]].concat();
+    
+        let mut s = VAULTY_PREFIX.to_owned();
+        s.push_str(&base64::encode(x));
+  
+        for r in s.as_bytes().chunks(80).map(str::from_utf8).collect::<Result<Vec<&str>, _>>().unwrap() {
+          println!("{}", r);
+        }
+      }
+      else {
+        eprintln!("Error: Password Verification Failed");
+      }
+    }
+    else if mo == 2{
+      let s: &String = &String::from_utf8(input).unwrap().split_whitespace().collect();
+
+      if s.starts_with(VAULTY_PREFIX) {
+        match base64::decode(&s[VAULTY_PREFIX.len()..]) {
+          Ok(v) => {
+            if v[0] == VAULTY_VERSION && v.len() > 29 {
+              let password = rpassword::prompt_password("Vaulty Password: ").unwrap();
+      
+              let mut key = [0_u8; 32];
+              let mut salt = [0_u8; 16];
+              salt.copy_from_slice(&v[1..17]);
+      
+              derive_key(&password, &mut salt, &mut key, false).unwrap();
+      
+              let mut nonce = [0_u8; 12];
+              nonce.copy_from_slice(&v[17..29]);
+      
+              let cipher = ChaCha20Poly1305::new(key[..].as_ref().into());
+              match cipher.decrypt(nonce[..].as_ref().into(), &v[29..]) {
+                Ok(v) => {
+                  std::io::stdout().write(&v).unwrap();
+                  std::io::stdout().flush().unwrap();
+                },
+                Err(_e) => {
+                  eprintln!("Error: Unable to Decrypt Ciphertext");
+                }
+              };
+            }
+            else {
+              eprintln!("Error: Invalid Vaulty Ciphertext");
+            }
+          },
+          Err(_e) => {
+            eprintln!("Error: Invalid Vaulty Ciphertext");
+          }
+        }
+      }
+      else {
+        eprintln!("Error: Invalid Vaulty Ciphertext");
+      }
     }
   }
   else {
-    let password = rpassword::prompt_password("Vaulty Password: ").unwrap();
-    if password == rpassword::prompt_password("Password Verification: ").unwrap() {
-      let mut key = [0_u8; 32];
-      let mut salt = [0_u8; 16];
-
-      derive_key(&password, &mut salt, &mut key, true).unwrap();
-
-      let mut nonce = [0_u8; 12];
-      OsRng.fill_bytes(&mut nonce);
-  
-      let cipher = ChaCha20Poly1305::new(key[..].as_ref().into());
-      let ciphertext = cipher.encrypt(nonce[..].as_ref().into(), &input[..]).unwrap();
-  
-      let x = [&VAULTY_VERSION.to_be_bytes(), &salt[..], &nonce[..], &ciphertext[..]].concat();
-  
-      let mut s = VAULTY_PREFIX.to_owned();
-      s.push_str(&base64::encode(x));
-
-      for r in s.as_bytes().chunks(80).map(str::from_utf8).collect::<Result<Vec<&str>, _>>().unwrap() {
-        println!("{}", r);
-      }
-    }
-    else {
-      eprintln!("Error: Password Verification Failed");
-    }
+    eprintln!("Vaulty v{}", env!("CARGO_PKG_VERSION"));
+    eprintln!("Usage: vaulty encrypt|decrypt");
   }
 }
-
+  
 fn derive_key(password: &str, salt: &mut [u8; 16], key: &mut [u8; 32], gsalt: bool) -> Result<(), scrypt::errors::InvalidOutputLen> {
   if gsalt == true {
     OsRng.fill_bytes(salt);
@@ -95,4 +123,4 @@ fn derive_key(password: &str, salt: &mut [u8; 16], key: &mut [u8; 32], gsalt: bo
   let params = scrypt::Params::new(16, 8, 1).unwrap();
   scrypt::scrypt(&password.as_bytes(), salt, &params, key)
 }
-
+  
