7267e85e682b19d25b323ebf9b6eccd864f81d59
Chris Mason <chris.mason@virginmediao2.co.uk>
2023-01-10 16:01:20 +0000

    add recursive sha256

--- src/main.rs
+++ src/main.rs
@@ -15,19 +15,34 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-use std::{io::{Read, Write}, str, cmp, env};
+use std::{io::{self, Read, Write}, fs::{File, metadata}, str, cmp, env};
 use chacha20poly1305::{aead::{Aead, KeyInit}, ChaCha20Poly1305};
 use rand_core::{RngCore, OsRng};
 use sha2::{Digest, Sha256};
+
+enum PolyIO {
+  Stdin(io::Stdin),
+  File(File)
+}
+
+impl io::Read for PolyIO {
+  fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+    match self {
+      PolyIO::Stdin(s) => s.read(buf),
+      PolyIO::File(f) => f.read(buf)
+    }
+  }
+}
 
 fn main() {
   const VAULTY_VERSION: u8 = 0x01;
   const VAULTY_PREFIX: &str = "$VAULTY;";
 
   let mut mo = 0;
-  let args: Vec<String> = env::args().collect();
+  let mut recurse = false;
+  let mut args: Vec<String> = env::args().collect();
 
-  if args.len() == 2 {
+  if args.len() > 1 {
     if args[1] == "encrypt"[..cmp::min(args[1].len(), 7)] {
       mo = 1;
     }
@@ -37,14 +52,18 @@ fn main() {
     else if args[1] == "sha256"[..cmp::min(args[1].len(), 6)] {
       mo = 3;
     }
+    if args.contains(&"-r".to_string()) {
+      args.retain(|x| *x != "-r");
+      recurse = true;
+    }
   }
 
   if mo > 0 {
-    let mut input = Vec::new();
-    let mut h = std::io::stdin().lock();
-    h.read_to_end(&mut input).unwrap();
-
     if mo == 1 {
+      let mut input = Vec::new();
+      let mut h = std::io::stdin().lock();
+      h.read_to_end(&mut input).unwrap();
+
       let password = rpassword::prompt_password("Vaulty Password: ").unwrap();
       if password == rpassword::prompt_password("Password Verification: ").unwrap() {
         let mut key = [0_u8; 32];
@@ -72,6 +91,10 @@ fn main() {
       }
     }
     else if mo == 2 {
+      let mut input = Vec::new();
+      let mut h = std::io::stdin().lock();
+      h.read_to_end(&mut input).unwrap();
+
       let s: &String = &String::from_utf8(input).unwrap().split_whitespace().collect();
 
       if s.starts_with(VAULTY_PREFIX) {
@@ -114,15 +137,63 @@ fn main() {
       }
     }
     else if mo == 3 {
-      let mut sha256 = Sha256::new();
-      sha256.update(input);
-      println!("{:x}", sha256.finalize());
+      if args.len() == 2 {
+        sha256(PolyIO::Stdin(std::io::stdin()), "-");
+      }
+      else {
+        let mut i = 2;
+        while i < args.len() {
+          let r = metadata(&args[i]);
+
+          if r.is_ok() && r.unwrap().is_dir() {
+            if recurse == true {
+              match std::fs::read_dir(&args[i]) {
+                Ok(d) => {
+                  for p in d {
+                    args.push(p.unwrap().path().display().to_string());
+                  }
+                },
+                Err(e) => {
+                  eprintln!("\x1b[1;31m{:<64}  {}\x1b[0m", e.to_string(), args[i]);
+                }
+              }
+            }
+          }
+          else {
+            match File::open(&args[i]) {
+              Ok(fh) => {
+                sha256(PolyIO::File(fh), &args[i]);
+              },
+              Err(e) => {
+                eprintln!("\x1b[1;31m{:<64}  {}\x1b[0m", e.to_string(), args[i]);
+              }
+            }
+          }
+          i += 1;
+        }
+      }
     }
   }
   else {
     eprintln!("Vaulty v{}", env!("CARGO_PKG_VERSION"));
-    eprintln!("Usage: vaulty encrypt|decrypt|sha256");
+    eprintln!("Usage: vaulty encrypt|decrypt");
+    eprintln!("              sha256 [-r] [file|dir] [..]");
   }
+}
+
+fn sha256(mut fh: PolyIO, f: &str) {
+  let mut buffer = [0; 4096];
+  let mut sha256 = Sha256::new();
+
+  while let Ok(n) = fh.read(&mut buffer[..]) {
+    if n > 0 {
+      sha256.update(&buffer[0..n]);
+    }
+    else {
+      break;
+    }
+  }
+  println!("{:x}  {}", sha256.finalize(), f);
 }
   
 fn derive_key(password: &str, salt: &mut [u8; 16], key: &mut [u8; 32], gsalt: bool) -> Result<(), scrypt::errors::InvalidOutputLen> {
